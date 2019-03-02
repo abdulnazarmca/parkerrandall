3f36036c1876a7d080faf8965fef0d46a43db12c
Abdul Nazar <40733227+abdulnazarmca@users.noreply.github.com>
2019-03-02 21:58:03 +0400

    Update Trading

--- pr_ethoscorp_trading_upgrades/__manifest__.py
+++ pr_ethoscorp_trading_upgrades/__manifest__.py
@@ -24,8 +24,10 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        #'views/views.xml',
-        #'views/templates.xml',
+	'views/purchase_order_view.xml',
+	'views/sale_order_view.xml',
+	'views/account_invoice_view.xml',
+
     ],
     # only loaded in demonstration mode
     'demo': [
