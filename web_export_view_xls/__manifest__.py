54e0d6956891720ad905a7009ba2abe15e32e9c7
Abdul Nazar <40733227+abdulnazarmca@users.noreply.github.com>
2019-04-11 12:13:03 +0400

    Add files via upload

---
+++ web_export_view_xls/__manifest__.py
@@ -0,0 +1,23 @@
+
+{
+    'name': 'Web Export Current View',
+    'version': '11.0.1.0.0',
+    'category': 'Web',
+    'author': 'AgaramSoft Team',
+    'live_test_url':'https://youtu.be/ClYRKY70rIg',
+    'website': 'https://agaramtechs.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'web',
+    ],
+    "data": [
+        'views/web_export_view_view.xml',
+    ],
+    'qweb': [
+        "static/src/xml/web_export_view_template.xml",
+    ],
+    'installable': True,
+    'auto_install': False,
+    'images':['static/description/Banner.png']
+}
+    
