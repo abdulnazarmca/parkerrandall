b1563151c7b33b8b8a4c83b6851dc5179779847f
Abdul Nazar <40733227+abdulnazarmca@users.noreply.github.com>
2019-05-26 15:04:42 +0400

    Update statement.py

--- account_partner_statement/wizard/statement.py
+++ account_partner_statement/wizard/statement.py
@@ -152,6 +152,8 @@ class PartnerStatementWizard(models.TransientModel):
             if not payment.currency_id and payment.amount_currency == 0:
                 amount_currency = amount
             amount = self.convert_rate(amount,payment.date,self.company_id.currency_id)
+            if payment.currency_id and payment.amount_currency != 0 and self.currency_id == payment.currency_id:
+                amount = payment.amount_currency
 
             info['content'] += [{
                 'name': payment.move_id.name,
